a33b01a9c0657cf11d57b1fad9c6614411d5c268
Emil Muratov <gpm@hotplug.ru>
2025-08-17 22:33:08 +0900

    change sdconfig options for yoradio

--- tools/archive-build.sh
+++ tools/archive-build.sh
@@ -33,8 +33,8 @@ rm -rf arduino-esp32/libraries/BLE
 rm -rf arduino-esp32/libraries/SimpleBLE
 rm -rf arduino-esp32/libraries/BluetoothSerial
 rm -rf arduino-esp32/libraries/WiFiProv
-rm -rf arduino-esp32/libraries/WiFiClientSecure
-rm -rf arduino-esp32/libraries/NetworkClientSecure
+#rm -rf arduino-esp32/libraries/WiFiClientSecure
+#rm -rf arduino-esp32/libraries/NetworkClientSecure
 rm -rf arduino-esp32/libraries/ESP_SR
 rm -rf arduino-esp32/libraries/ESP_NOW
 rm -rf arduino-esp32/libraries/TFLiteMicro
